a37677bcb0bd5bd73523e9a6fbcf66406297729a
abode118 <46899622+abode118@users.noreply.github.com>
2019-11-02 11:52:41 -0400

    Update CorporateTaxes.py

--- Code by tax type/CorporateTaxes.py
+++ Code by tax type/CorporateTaxes.py
@@ -1,37 +1,21 @@
-#Corporate Tax functions for Medicare for All Project
 """
 Created on Sat Sep 21 10:48:01 2019
 
 @author: abode
 """
+
 import csv
 
 """
-Function: create dictionary of dictionaries for Federal Revenue Data by Year;
-note that 2019-2024 are estimates
-
-Inputs:
-    RevFile, a csv file
-Output: dictionary of dictionaries, format below:
-{RevenueBucket:
-    Year:
-        Revenue}
+******************************************************************************
+FUNCTIONS NEEDED TO PROJECT CORPORATE TAX REVENUE
+******************************************************************************
 """
 
-def CreateFedRevDict(RevFile):
-    with open(RevFile) as f:
-        FedRevList = list(list(yr) for yr in csv.reader(f, delimiter=','))
-        f.close()
-    DictFedRev = {}
-    for i in range(1,4):
-        DictFedRev[FedRevList[0][i]] = {}
-    for yr in FedRevList[1:]:
-        for i in range(1,4):
-            yr[i] = int(yr[i])
-            DictFedRev[FedRevList[0][i]][str(yr[0])] = yr[i]
-    return DictFedRev
-
 """
+******************************************************************************
+PROJECTCORPORATETAX()
+
 Function: Projecting corporate tax, given a current rate and new rate (can be
 the same, if you're looking for a current state projection)
 
@@ -46,6 +30,7 @@ Inputs:
     GDPGrowth, a float
 
 Output: a list of corporate tax revenue
+******************************************************************************
 """
 def ProjectCorporateTax(FedRevFile,BaseYear,StartYear,Years,CurrentCorpTaxRate,
                         NewCorpTaxRate,Inflation,GDPGrowth):
@@ -63,10 +48,48 @@ def ProjectCorporateTax(FedRevFile,BaseYear,StartYear,Years,CurrentCorpTaxRate,
         CorpIncome = CorpIncome * (1 + GDPGrowth)
         CorpIncome = CorpIncome * (1 + Inflation)
         
-    
     return CorpTaxRev
 
+
 """
+******************************************************************************
+HELPER CODE
+******************************************************************************
+"""
+
+
+"""
+******************************************************************************
+CREATEFEDREVDICT()
+
+Function: create dictionary of dictionaries for Federal Revenue Data by Year;
+note that 2019-2024 are estimates
+
+Inputs:
+    RevFile, a csv file
+Output: dictionary of dictionaries, format below:
+{RevenueBucket:
+    Year:
+        Revenue}
+******************************************************************************
+"""
+def CreateFedRevDict(RevFile):
+    with open(RevFile) as f:
+        FedRevList = list(list(yr) for yr in csv.reader(f, delimiter=','))
+        f.close()
+    DictFedRev = {}
+    for i in range(1,4):
+        DictFedRev[FedRevList[0][i]] = {}
+    for yr in FedRevList[1:]:
+        for i in range(1,4):
+            yr[i] = int(yr[i])
+            DictFedRev[FedRevList[0][i]][str(yr[0])] = yr[i]
+    return DictFedRev
+
+"""
+******************************************************************************
+RAISECORPORATETAX()
+
 Function: raise corporate tax rate
 
 Inputs:
@@ -74,7 +97,7 @@ Inputs:
     increment, a float
 
 Output: new corporate tax rate (current + increment), a float
+******************************************************************************
 """
-
 def RaiseCorporateTax(CurrentCorpTaxRate,increment):
     return round(CurrentCorpTaxRate + increment,4)
